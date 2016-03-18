74e9f572fb9399611f079726e27194e23b5d31ae
jadijadi <jadijadi@gmail.com>
2016-03-18 16:53:39 +0330

    added the web app

---
+++ web/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Task
+# Register your models here.
+
+admin.site.register(Task)
---
+++ web/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    #url(r'^login$', views.login_page, name='login_page'),
+    #url(r'^logout$', views.logout_page, name='logout_page'),
+]
