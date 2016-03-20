0d6ab61f059ab9903b75787016aa72f8aa2b54d8
jadijadi <jadijadi@gmail.com>
2016-03-20 23:32:41 +0330

    done and redo buttons. add task form. limiting login attempts to 1 per 3 secconds

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^login$', views.login_page, name='login_page'),
-    #url(r'^logout$', views.logout_page, name='logout_page'),
+    url(r'^accounts/login/$', views.login_page, name='login_page'),
+    url(r'^accounts/logout/$', views.logout_page, name='logout_page'),
+    url(r'^task/([0-9]+)/done/$', views.taskdone, name='taskdone'),
+    url(r'^task/([0-9]+)/redo/$', views.taskredo, name='taskredo'),
+    url(r'^task/add/$', views.taskadd, name='taskadd'),
 ]
