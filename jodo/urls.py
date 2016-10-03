1205239b3f60b5491b07feaa5c435623fec58fb1
jadijadi <jadijadi@gmail.com>
2016-10-03 13:48:44 +0330

    upgraded to django 1.10

--- jodo/urls.py
+++ jodo/urls.py
@@ -1,14 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'jodo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('web.urls')),
-
-)
+]
